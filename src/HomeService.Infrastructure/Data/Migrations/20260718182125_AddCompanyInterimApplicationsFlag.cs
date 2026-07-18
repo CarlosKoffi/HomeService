3fff92b7ecd8d9b663e66d273572c579f6db0b18
Carlos Amorin <bruce.carl@gmail.com>
2026-07-18 20:25:58 +0200

    Add EF migration for interim company flag

---
+++ src/HomeService.Infrastructure/Data/Migrations/20260718182125_AddCompanyInterimApplicationsFlag.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeService.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCompanyInterimApplicationsFlag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AcceptsInterimApplications",
+                table: "Companies",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AcceptsInterimApplications",
+                table: "Companies");
+        }
+    }
+}
